0be0da9aed64b84f908856bacb3906398f9eb3d3
El-gibbor <chiagoziem11@yahoo.com>
2024-09-30 16:13:02 +0200

    Revert "print line after after the expected word"
    This reverts commit 5cf7c037a6524f64b04d2936ea8eb0974ba1eed1.

--- src/routes/login.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/login")({
-  component: () => (
-    <div className="w-full h-screen bg-[url('public/shopping-cat.png')] bg-cover bg-center">
-      {/* Your page content goes here */}
-    </div>
-  ),
-});
